cc12638211eb889c20403b3052e9086601ca085a
Notchine <aelmiet@hotmail.com>
2024-01-25 17:24:19 +0000

    started thingie

---
+++ main.py
@@ -0,0 +1,8 @@
+#opening, mosting just set dressing with no real key info to keep track of this early in the game
+print("You stand at the precipice of the vampires castle, the screeching ")
+print("of bats echoing as the door slides open on its own.")
+print("You have one single task. Defeat the vampire within, finally setting your village free from his darkness.")
+#blank text line for separation between the dialogue, and actual interaction/gameplay
+print("")
+print("You can either ENTER the caslte, or OBSERVE your surroundings before entering.")
+print("What will you do? (Use the capitalised words above as your inputs for the player!)")
